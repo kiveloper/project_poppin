168ca065d639f8960415c7da67e9ba2c82cfd3f2
Kim <kimgyudo93@gmail.com>
2024-06-25 16:40:45 +0900

    nav_list tag update

--- lib/pages/popup_list_nav_page.dart
+++ lib/pages/popup_list_nav_page.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:flutter/material.dart';
@@ -23,6 +24,7 @@ class _PopUpListNavPageState extends State<PopUpListNavPage>
   GlobalKey upScrollPosition = GlobalKey();
   GlobalKey downScrollPosition = GlobalKey();
   double hashTagSize = 100;
+  bool _isButtonDisabled = false;
   bool endedPopUpState = prefs.getBool("endedPopUpState")!;
   bool clickStop = false;
   bool hashTagExtendState = false;
@@ -148,34 +150,7 @@ class _PopUpListNavPageState extends State<PopUpListNavPage>
                                     storeController.storeAllTagList.length,
                                     (index) {
                                   return ActionChip(
-                                    onPressed: () {
-                                      if (storeController.hashTageSetting ==
-                                          storeController
-                                              .storeAllTagList[index]) {
-                                        storeController.setHashTagSetting("");
-                                        storeController.getNavPageStoreAllList(
-                                            endedPopUpState);
-                                        setState(() {
-                                          if (hashTagSize != 100) {
-                                            hashTagSize = 100;
-                                            hashTagExtendState = false;
-                                          }
-                                        });
-                                      } else {
-                                        storeController.setHashTagSetting(
-                                            storeController
-                                                .storeAllTagList[index]);
-                                        storeController.getHashTagStoreDateList(
-                                            storeController.hashTageSetting,
-                                            endedPopUpState);
-                                        setState(() {
-                                          if (hashTagSize != 100) {
-                                            hashTagSize = 100;
-                                            hashTagExtendState = false;
-                                          }
-                                        });
-                                      }
-                                    },
+                                    onPressed: _isButtonDisabled ? null : () => _onPressed(storeController, index, endedPopUpState),
                                     label: Text(
                                       "#${storeController.storeAllTagList[index]}",
                                       style: const TextStyle(
@@ -360,6 +335,35 @@ class _PopUpListNavPageState extends State<PopUpListNavPage>
       // 사용자가 아래로 스크롤하는 중
       Scrollable.ensureVisible(downScrollPosition.currentContext!,
           duration: const Duration(milliseconds: 150), alignment: 0);
+    }
+  }
+
+  void _onPressed(StoreController storeController, int index, bool endedPopUpState) {
+    if (!_isButtonDisabled) {
+      setState(() {
+        _isButtonDisabled = true;
+      });
+
+      Timer(const Duration(milliseconds: 1000), () {
+        setState(() {
+          _isButtonDisabled = false;
+        });
+      });
+
+      if (storeController.hashTageSetting == storeController.storeAllTagList[index]) {
+        storeController.setHashTagSetting("");
+        storeController.getNavPageStoreAllList(endedPopUpState);
+      } else {
+        storeController.setHashTagSetting(storeController.storeAllTagList[index]);
+        storeController.getHashTagStoreDateList(storeController.hashTageSetting, endedPopUpState);
+      }
+
+      setState(() {
+        if (hashTagSize != 100) {
+          hashTagSize = 100;
+          hashTagExtendState = false;
+        }
+      });
     }
   }
 }
